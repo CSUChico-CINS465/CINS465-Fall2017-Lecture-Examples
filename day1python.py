fef5671325cd659f30d019fd01a1a72d74a8113a
Bryan Dixon <javawolfpack@gmail.com>
2017-08-28 10:49:02 -0700

    Day 1 Python Code

---
+++ day1python.py
@@ -0,0 +1,43 @@
+print("Hello World")
+
+# Python 2 Hello World
+# print "Hello World"
+
+x = 4
+y = 5
+# print(x+y)
+# print(x/y)
+# print(x*y)
+# print(x//y)
+# print(x-y)
+# print(y%x)
+#print(x**y)
+# print((1+3)*2)
+
+# if not True:
+#     if True:
+#         print(True)
+# else:
+#     print(False)
+
+# if 1!=2:
+#     print ("Hi")
+
+# if not "He" in "Hello":
+#     print(True)
+
+# a = []
+#
+# for x in range(20):
+#     a=a+[x+1]
+
+#print(len(a))
+#print(range(len(a)))
+# for x in range(len(a)):
+#     #print(x)
+#     a[x]=a[x]+1
+#
+# print(a)
+
+a = [1,"2",3]
+print(a)
