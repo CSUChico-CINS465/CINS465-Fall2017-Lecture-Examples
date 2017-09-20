a77a2ef23dc33c87dcecc20bfc19392f3753f415
Bryan Dixon <javawolfpack@gmail.com>
2017-09-20 10:51:50 -0700

    Added Login

--- mysite/firstapp/forms.py
+++ mysite/firstapp/forms.py
@@ -1,5 +1,22 @@
 from django import forms
 from django.core.validators import validate_unicode_slug
 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
 class suggestion_form(forms.Form):
     suggestion = forms.CharField(label='Suggestion', max_length=140, validators=[validate_unicode_slug])
+
+class LoginForm(AuthenticationForm):
+    username=forms.CharField(
+        label="Username",
+        max_length=30,
+        widget=forms.TextInput(attrs={
+            'name':'username'
+        })
+    )
+    password=forms.CharField(
+        label="Password",
+        max_length=32,
+        widget=forms.PasswordInput()
+    )
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.contrib.auth import views
+from firstapp.forms import LoginForm
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'login/$', views.login, {
+        'template_name':'login.html',
+        'authentication_form':LoginForm
+    }, name="login"),
+    url(r'logout/$', views.logout,{
+        'next_page':'/login'
+    }),
     url(r'',include('firstapp.urls')),
 ]
