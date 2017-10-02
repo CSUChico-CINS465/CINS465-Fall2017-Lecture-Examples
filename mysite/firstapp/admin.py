93cfd3dcaddacf12868b9e1148426b8fc79c1c39
Bryan Dixon <javawolfpack@gmail.com>
2017-10-02 10:50:58 -0700

    Advanced models

--- mysite/firstapp/admin.py
+++ mysite/firstapp/admin.py
@@ -4,3 +4,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(suggestion)
+admin.site.register(comment)
--- mysite/firstapp/models.py
+++ mysite/firstapp/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class suggestion(models.Model):
     suggestion = models.CharField(max_length=141)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    authored_on = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.suggestion
+
+class comment(models.Model):
+    comment = models.CharField(max_length=141)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    suggestion = models.ForeignKey(suggestion, on_delete=models.CASCADE)
+    authored_on = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
